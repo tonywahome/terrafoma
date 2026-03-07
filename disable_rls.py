4386e761108135af96b12d367a2ba0a05f87d899
fnyoni-dotcom <f.nyoni@alustudent.com>
2026-03-07 12:26:21 +0200

    Update disable_rls.py

--- disable_rls.py
+++ disable_rls.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Disable Row Level Security on all tables for development"""
+"""Disable Row Level Security on all tables for development"""]
+
+# project
 
 import os
 import requests
